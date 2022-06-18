70d9224cc8494802d8b92f0c4dfa798a0ce1639e
Nirklav <mrAnton911@gmail.com>
2022-06-18 11:44:05 +0300

    CopyCutPasteController was fixed.

--- src/ui/copy_cut_paste_controller.rs
+++ src/ui/copy_cut_paste_controller.rs
@@ -1,21 +1,20 @@
-use druid::{commands, Data, Env, Event, EventCtx, Target, Widget};
+use druid::{Code, commands, Data, Env, Event, EventCtx, Target, Widget};
 use druid::widget::Controller;
-use druid_shell::{HotKey, SysMods};
 
 pub struct CopyCutPasteController;
 
 impl<T: Data, W: Widget<T>> Controller<T, W> for CopyCutPasteController {
     fn event(&mut self, child: &mut W, ctx: &mut EventCtx, event: &Event, data: &mut T, env: &Env) {
         match event {
-            Event::KeyDown(k_e) if HotKey::new(SysMods::Cmd, "c").matches(k_e) => {
+            Event::KeyDown(k_e) if k_e.code == Code::KeyC && k_e.mods.ctrl() => {
                 ctx.submit_command(commands::COPY.to(Target::Auto));
                 ctx.set_handled();
             }
-            Event::KeyDown(k_e) if HotKey::new(SysMods::Cmd, "x").matches(k_e) => {
+            Event::KeyDown(k_e) if k_e.code == Code::KeyX && k_e.mods.ctrl() => {
                 ctx.submit_command(commands::CUT.to(Target::Auto));
                 ctx.set_handled();
             }
-            Event::KeyDown(k_e) if HotKey::new(SysMods::Cmd, "v").matches(k_e) => {
+            Event::KeyDown(k_e) if k_e.code == Code::KeyV && k_e.mods.ctrl() => {
                 ctx.submit_command(commands::PASTE.to(Target::Auto));
                 ctx.set_handled();
             }
